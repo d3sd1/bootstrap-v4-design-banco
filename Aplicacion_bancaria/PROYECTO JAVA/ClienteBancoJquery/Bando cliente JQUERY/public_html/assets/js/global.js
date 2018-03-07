a1a3d69dd1f0e4ddf1adb8070977d28a733393b3
Jause <andirexulon@gmail.com>
2018-03-07 20:50:42 +0100

    - Añadido interceptor a ajax calls.

--- Aplicacion_bancaria/PROYECTO JAVA/ClienteBancoJquery/Bando cliente JQUERY/public_html/assets/js/global.js
+++ Aplicacion_bancaria/PROYECTO JAVA/ClienteBancoJquery/Bando cliente JQUERY/public_html/assets/js/global.js
@@ -1,4 +1,12 @@
 const API_REST_URL = "http://localhost:8000";
+$.ajaxSetup({
+    error: function (xhr) {
+        if(xhr.status === 407) //token caducado!!
+        {
+            logout();
+        }
+    }
+});
 function isUserConnected()
 {
     var token = localStorage.getItem("token");
